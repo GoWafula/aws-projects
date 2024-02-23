fb450ec15959cc172a8be0012ba84f3f139107d1
Kevin Wafula <119099815+GoWafula@users.noreply.github.com>
2024-02-23 04:23:03 -0500

    small change

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull GoWafula/simple-python-flask-app
+docker pull gowafula/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 GoWafula/simple-python-flask-app
+docker run -d -p 5000:5000 gowafula/simple-python-flask-app
